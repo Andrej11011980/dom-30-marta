eb39e191ac7a11a898f4f598cefdff8026c27050
Андрей Боярский <an0982978490@outlook.com>
2022-03-30 10:42:41 +0300

    Добавьте файлы проекта.

---
+++ dom 30 marta/dom 30 marta.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+
+void SetCursor(int y, int x, int color) {
+
+    HANDLE h = GetStdHandle(-11);
+    COORD cursor;
+    cursor.Y = y;
+    cursor.X = x;
+    SetConsoleTextAttribute(h, color);
+    SetConsoleCursorPosition(h, cursor);
+}
+
+int main()
+{
+    SetCursor(5, 7, 12);
+
+    cout << "cursor operation" << "\n";
+}
